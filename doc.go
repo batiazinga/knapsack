897fc0e64bb2eec0eaf851b0fd4628bde2002165
Batiazinga <luc.nguyenthe@gmail.com>
2018-02-03 15:42:17 +0100

    Document package

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Package knapsack provides algorithms to solve the 0-1 Knapsack problem with integer values and weights.
+
+See https://en.wikipedia.org/wiki/Knapsack_problem for a description of the problem.
+Note that we assume that capacity, weights and values are strictly positive integers.
+*/
+package knapsack
